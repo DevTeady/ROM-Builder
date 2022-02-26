9ae129d209a2293b36606f7a55d40212cd23c035
David T Eady <61388628+DevTeady@users.noreply.github.com>
2022-02-26 10:17:34 +0600

    Update apts.sh

--- apts.sh
+++ apts.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-apt update && upgrade -y && apt install -y git ssh apt-utils sudo erofs-utils
+sudo apt update && upgrade -y && apt install -y git ssh apt-utils sudo erofs-utils
 
 git config --global user.name $GIT_USERNAME &&  git config --global user.email $GIT_EMAIL
 
-rm -f /etc/profile ~/.profile ~/.bashrc
+sudo rm -f /etc/profile ~/.profile ~/.bashrc
 
 git clone https://github.com/Sushrut1101/Docker && cd Docker && cp -r proprietary/* /
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata && ln -sf /usr/share/zoneinfo/$TZ /etc/localtime && apt-get install -y tzdata && dpkg-reconfigure --frontend noninteractive tzdata
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends tzdata && ln -sf /usr/share/zoneinfo/$TZ /etc/localtime && apt-get install -y tzdata && dpkg-reconfigure --frontend noninteractive tzdata
 
 sudo apt install curl wget aria2 tmate python2 python3 silversearch* iputils-ping iproute2 nano rsync rclone tmux screen openssh-server python3-pip adb fastboot jq npm neofetch mlocate zip unzip tar ccache cpio lzma schedtool openjdk-8-jdk -y
 

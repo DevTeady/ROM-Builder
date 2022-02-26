cab475001e55105602a27880d8d4972ef1711070
David T Eady <61388628+DevTeady@users.noreply.github.com>
2022-02-26 09:25:11 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd $SYNC_PATH
 
 # Set-up ccache
 if [ -z "$CCACHE_SIZE" ]; then
-    ccache -M 10G
+    ccache -M 50G
 else
     ccache -M ${CCACHE_SIZE}
 fi
@@ -20,13 +20,7 @@ source build/envsetup.sh
 lunch ${LUNCH_COMBO} || { echo "ERROR: Failed to lunch the target!" && exit 1; }
 
 # Build the Code
-if [ -z "$J_VAL" ]; then
-    mka -j$(nproc --all) $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
-elif [ "$J_VAL"="0" ]; then
-    mka $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
-else
-    mka -j${J_VAL} $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
-fi
+make bacon -j$(nproc --all)
 
 # Exit
 exit 0
